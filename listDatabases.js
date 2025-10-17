3804e5fc897e862d579273baf191ae7ab8773812
jlg777 <proyectojlg777@gmail.com>
2025-10-17 15:48:26 -0300

    mongo portable

--- listDatabases.js
+++ listDatabases.js
@@ -20,3 +20,12 @@ async function listDatabases() {
 }
 
 listDatabases();
+
+
+//https://www.mongodb.com/try/download/community
+//Descargá y descomprimí el ZIP en una carpeta de tu elección, por ejemplo:
+//C:\MongoDB\
+//En la carpeta donde descomprimiste MongoDB, creá estas subcarpetas manualmente:
+//C:\MongoDB\bin
+//C:\MongoDB\data\db
+//C:\MongoDB\logs
