d730372e7d6208e4acd7232daf88b1dc956a41a8
Jorge Grandia <jgrandia@nucetemza.com>
2025-11-07 13:16:13 -0300

    console error

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -18,9 +18,10 @@ export default function auth(req, res, next) {
 
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
-    req.user = decoded; 
+    req.user = decoded;
     next();
   } catch (err) {
-    return res.status(403).json({ error: "Token inválido o expirado" });
+    console.error("Error al verificar token:", err.message);
+    return res.status(403).json({ error: "Token inválido o expirado"});
   }
 }
