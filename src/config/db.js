1bbf85ee168b840cda0f94f266b5d00cf60b7c74
Jorge Grandia <jgrandia@nucetemza.com>
2025-12-12 13:11:32 -0300

    update

--- src/config/db.js
+++ src/config/db.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    //const MONGO_URI = process.env.MONGO_URI;
-    const MONGO_LOCAL = process.env.MONGO_LOCAL;
-    await mongoose.connect(MONGO_LOCAL);
+    const MONGO_URI = process.env.MONGO_URI;
+    //const MONGO_LOCAL = process.env.MONGO_LOCAL;
+    await mongoose.connect(MONGO_URI);
     console.log("✅ Conectado a MongoDB Atlas");
   } catch (error) {
     console.error("❌ Error al conectar a MongoDB:", error);
